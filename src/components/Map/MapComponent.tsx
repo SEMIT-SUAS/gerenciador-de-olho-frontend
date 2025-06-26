6328e4d10948e2d566eb01c5e669a3bcbc9cc8d0
Fabio <fabiocaldas266@gmail.com>
2025-06-26 13:11:34 -0300

    melhoria do código

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -16,17 +16,26 @@ interface MapComponentProps {
     detailViewItem: Denuncia | Acao | null;
 }
 
-export const MapComponent: FC<MapComponentProps> = ({ denuncias, acoes, modoSelecao, denunciasSelecionadas, onMarkerClick, onSelectionClick, detailViewItem }) => {
+export function MapComponent({
+    denuncias,
+    acoes,
+    modoSelecao,
+    denunciasSelecionadas,
+    onMarkerClick,
+    onSelectionClick,
+    detailViewItem
+}: MapComponentProps) {
+    
     const MapViewUpdater: FC<{ item: Denuncia | Acao | null }> = ({ item }) => {
         const map = useMap();
-        useEffect(() => {     
+        useEffect(() => {
             if (item) {
                 map.flyTo([item.lat, item.lon], 16);
             }
-        }, [item, map]); 
+        }, [item, map]);
         return null;
     }
-   
+
     const MapCursor: FC<{ modoSelecao: boolean }> = ({ modoSelecao }) => {
         const map = useMap();
         useEffect(() => {
@@ -36,7 +45,7 @@ export const MapComponent: FC<MapComponentProps> = ({ denuncias, acoes, modoSele
     }
 
     const handleMarkerClick = (item: Denuncia | Acao) => {
-        if(modoSelecao && 'status' in item && item.status === 'aberto') {
+        if (modoSelecao && 'status' in item && item.status === 'aberto') {
             onSelectionClick(item.id, item.status);
         } else if (!modoSelecao) {
             onMarkerClick(item);
@@ -51,24 +60,24 @@ export const MapComponent: FC<MapComponentProps> = ({ denuncias, acoes, modoSele
                 attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
                 url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=XLhcrfhE5GT4MmbYP817"
             />
-            
+
             {denuncias.map(d => {
                 const isSelected = modoSelecao && denunciasSelecionadas.includes(d.id);
                 let icon;
                 if (d.status === 'aberto') icon = isSelected ? iconDenunciaSelecionada : iconDenuncia;
                 else if (d.status === 'em_andamento') icon = iconDenunciaEmAtendimento;
-                
+
                 return (
                     <Marker key={`d-${d.id}`} position={[d.lat, d.lon]} icon={icon} eventHandlers={{ click: () => handleMarkerClick(d) }}>
-                        {!modoSelecao && <Popup><b>Denúncia:</b> {d.titulo}<br/><b>Status:</b> <span className="capitalize">{d.status.replace('_', ' ')}</span></Popup>}
+                        {!modoSelecao && <Popup><b>Denúncia:</b> {d.titulo}<br /><b>Status:</b> <span className="capitalize">{d.status.replace('_', ' ')}</span></Popup>}
                     </Marker>
                 );
             })}
-            
+
             {acoes.map(a => (
                 <React.Fragment key={`acao-group-${a.id}`}>
                     <Marker key={`a-${a.id}`} position={[a.lat, a.lon]} icon={iconAcao} eventHandlers={{ click: () => handleMarkerClick(a) }}>
-                        {!modoSelecao && <Popup><b>Ação:</b> {a.nome}<br/><b>Secretaria:</b> {a.secretaria}</Popup>}
+                        {!modoSelecao && <Popup><b>Ação:</b> {a.nome}<br /><b>Secretaria:</b> {a.secretaria}</Popup>}
                     </Marker>
                     {a.polygonCoords.length > 0 && <Polygon pathOptions={{ color: 'green', weight: 2, fillOpacity: 0.1 }} positions={a.polygonCoords} />}
                 </React.Fragment>
