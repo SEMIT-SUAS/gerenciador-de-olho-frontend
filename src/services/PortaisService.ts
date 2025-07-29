bde1f051702689b86112ebc3f72223a1e8be8c1b
mrkevyn <85470079+mrkevyn@users.noreply.github.com>
2025-07-29 15:38:59 -0300

    Update PortaisService.ts
    att

--- src/services/PortaisService.ts
+++ src/services/PortaisService.ts
@@ -2,41 +2,40 @@ import type { Portais } from "../types/Portais";
 import { API_BASE_URL } from "../config/api";
 
 export async function getAllPortais(): Promise<Portais[]> {
-    try{
-        const response = await fetch(`${API_BASE_URL}/portal/listar-ativos`, {
-            method: 'GET',
-        })
+  try{
+      const response = await fetch(`${API_BASE_URL}/portal/listar-ativos`, {
+          method: 'GET',
+      })
 
-        if(!response.ok){
-            throw new Error('Não foi possível listar os serviços.')
-        }
+      if(!response.ok){
+          throw new Error('Não foi possível listar os serviços.')
+      }
 
-        return await response.json()
-    } catch (error){
-        throw new Error('Infelizmente ocorreu um erro no servidor. Tente novamente mais tarde')
-    }    
+      return await response.json()
+  } catch (error){
+      throw new Error('Infelizmente ocorreu um erro no servidor. Tente novamente mais tarde')
+  }    
 }
 
 export async function createPortal(portal: Portais): Promise<Portais> {
-    try {
-        const response = await fetch(`${API_BASE_URL}/portal/cadastrar`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(portal),
-        })
+  try {
+    const response = await fetch(`${API_BASE_URL}/portal/cadastrar`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(portal),
+      })
 
-        if (!response.ok){
-            throw new Error('Não foi possível salvar serviço')
-        }
-        return await response.json()
-    } catch (error){
-        throw new Error('Infelizmente ocorreu um erro no servidor. Tente novamente')
-    }    
+    if (!response.ok){
+      throw new Error('Não foi possível salvar serviço')
+    }
+      return await response.json()
+  } catch (error){
+      throw new Error('Infelizmente ocorreu um erro no servidor. Tente novamente')
+  }    
 }
 
 export async function toggleAtivo(id: number, ativo: boolean): Promise<void> {
   try {
-    console.log(id, ativo);
     const response = await fetch(`${API_BASE_URL}/portal/atualizar/atividade`, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
@@ -48,13 +47,13 @@ export async function toggleAtivo(id: number, ativo: boolean): Promise<void> {
     }
   } catch (error) {
     console.error('Erro na requisição toggleAtivo:', error);
-    throw error; // repassa para ser tratado no componente, se necessário
+    throw error;
   }
 }
 
 export async function changeServiceVisibility(id: number, visivel: boolean): Promise<void> {
   try {
-    const response = await fetch(`${API_BASE_URL}/atualizar/visibilidade`, {
+    const response = await fetch(`${API_BASE_URL}/portal/atualizar/visibilidade`, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ id, visivel }),
