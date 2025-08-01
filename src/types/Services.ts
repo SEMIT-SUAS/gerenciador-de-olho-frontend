da091af5828be6e562c4723831fbf9f1830e3fbc
Lorena <lorenaloureiro39@gmail.com>
2025-08-01 17:45:15 -0300

    alterando a função service

--- src/types/Services.ts
+++ src/types/Services.ts
@@ -19,6 +19,32 @@ export interface Services {
   prioridades: string
   horarioAtendimento: string
   legislacao: string
+  nomeCategoria: string,
+  setorLotacao: string
+  modeloRequerimento: string
+  nomesPersonas?: string[],
+  visivel: boolean
+  ativo: boolean
+  nomeOrgao?: string;
+}
+
+export interface CreateService {
+  orgao: number | null;
+  secretaria: SecretariaModel | null
+  id: number | null;
+  nome: string
+  descricao: string
+  publicoDestinado: string[]
+  formasSolicitacao: string[]
+  documentacaoNecessaria: string[]
+  custos: string
+  etapas: string
+  requisitos: string
+  formasAcompanhamento: string
+  prazoAtendimento: string
+  prioridades: string
+  horarioAtendimento: string
+  legislacao: string
   categoria: ServicoCategoria | null;
   setorLotacao: string
   modeloRequerimento: string
