d0ea5b6e6b66223355342b725e73743f45a11664
mrkevyn <kevyn.carlos@hotmail.com>
2025-08-06 16:44:27 -0300

    att

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -10,26 +10,26 @@ import { ServicoNovo } from "../pages/services/ServicoNovo";
 import { ServicoEditar } from "../pages/services/ServicoEditar";
 import{ ServicoExternoList } from "../pages/ServicoExterno/ServicoExternoList"
 import { SecretariaPage } from "../pages/secretaria/SecretariaPage";
-import { EspacoPublicoForm } from "../pages/espacoPublico/EspacoPublicoForm";
-import { EspacoPublicoList } from "../pages/espacoPublico/EspacoPublicoList";
+import { EspacoPublicoForm } from "../pages/espacoPublico/EspacoPublicoCadastrar";
+import { EspacoPublicoList } from "../pages/espacoPublico/EspacoPublicoPage";
 import { EspacoPublicoEdit } from "../pages/espacoPublico/EspacoPublicoEdit";
 
 export function Navigator() {
   return (
     <Routes>
       <Route index element={<OcorrenciasPage />} />
-      <Route path="/categorias" element={<ListaCategorias />} />
-      <Route path="/portais" element={<PortaisList />} />
-      <Route path="/banner" element={<BannerList />} />
-      <Route path="/servicos" element={<ServicesList />} />
-      <Route path="/servico/novo" element={<ServicoNovo />} />
-      <Route path="/servico/:id" element={<ServicoDetalhes />} />
-      <Route path="/servico/editar/:id" element={<ServicoEditar />} />
+      <Route path="/categorias" element={<ListaCategorias/>} />
+      <Route path="/portais" element={<PortaisList/>} />
+      <Route path="/banner" element={<BannerList/>} />
+      <Route path="/servicos" element={<ServicesList/>} />
+      <Route path="/servico/novo" element={<ServicoNovo/>} />
+      <Route path="/servico/:id" element={<ServicoDetalhes/>} />
+      <Route path="/servico/editar/:id" element={<ServicoEditar/>} />
       <Route path="/servico-externo" element={<ServicoExternoList/>}/>
       <Route path="/secretaria" element={<SecretariaPage/>}/>
       <Route path="/espaco-publico-cadastrar" element={<EspacoPublicoForm/>}/>
       <Route path="/espaco-publico" element={<EspacoPublicoList/>}/>
-      <Route path="/espaco-publico-editar/:id" element={<EspacoPublicoEdit />} /> 
+      <Route path="/espaco-publico-editar/:id" element={<EspacoPublicoEdit/>} /> 
     </Routes>
   );
 }
